943b6d065edfe94134d38da5141178f77fdfc354
EdiJunior88 <er.moura88@yahoo.com.br>
2022-11-08 19:30:40 -0300

    Aula 01 - a Web, os Componentes e o deploy!

---
+++ pages/index.js
@@ -0,0 +1,87 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
+function HomePage() {
+  return (
+    <>
+      <CSSReset />
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+        }}>
+        <Menu />
+        <Header />
+        <Timeline playlists={config.playlists}>Conteúdo</Timeline>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  .user-info {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+
+function Header() {
+  return (
+    <StyledHeader>
+      <section className='user-info'>
+        <img src={`https://github.com/${config.github}.png`} />
+
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+}
+
+function Timeline(propriedades) {
+  const playlistsNames = Object.keys(propriedades.playlists);
+
+  return (
+    <StyledTimeline>
+      {playlistsNames.map((playlistsNames, id) => {
+        const videos = propriedades.playlists[playlistsNames];
+
+        return (
+          <section key={id}>
+            <h2>{playlistsNames}</h2>
+
+            <div>
+              {videos.map((video, id2) => {
+                return (
+                  <a href={video.url} key={id2}>
+                    <img src={video.thumb} />
+                    <span>{video.title}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
+}
