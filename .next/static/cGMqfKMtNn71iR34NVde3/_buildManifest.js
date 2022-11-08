81e8420e9515b1ac77b6c55833ee36f667285813
EdiJunior88 <er.moura88@yahoo.com.br>
2022-11-08 19:45:56 -0300

    update

--- .next/static/cGMqfKMtNn71iR34NVde3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/332-1cc38d134edbcbf7.js","static/chunks/pages/index-7e6288afdeb60272.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
